9a2d2998305e3c17ce1b3de55e2cfa5413fb0d0c
Ao <a6o5a3@gmail.com>
2026-08-21 00:28:38 +0900

    add prosopite

--- Gemfile
+++ Gemfile
@@ -60,4 +60,7 @@ group :development, :test do
   gem "rubocop-factory_bot", require: false
   gem "rubocop-performance", require: false
   gem "rubocop-rake", require: false
+
+  gem "prosopite"
+  gem "pg_query"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -89,4 +89,12 @@ RSpec.configure do |config|
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
   # Kernel.srand config.seed
+
+  config.before do
+    Prosopite.scan
+  end
+
+  config.after do
+    Prosopite.finish
+  end
 end
